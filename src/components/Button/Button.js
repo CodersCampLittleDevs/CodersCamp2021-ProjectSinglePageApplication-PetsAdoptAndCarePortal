4acdd1da3f7a5c15680e06909b51c62f09555693
Alicja Guzik <95084583+alicjaguzik@users.noreply.github.com>
2022-01-19 22:14:15 +0100

    fix: Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import propTypes from "prop-types";
 
-export const Button = ({ to, children, type, styleClass, onClick }) => {
+export const Button = ({
+  to,
+  children,
+  type,
+  styleClass,
+  onClick,
+  disabled,
+}) => {
   if (to) {
     return <Link to={to}>{children}</Link>;
   }
@@ -10,6 +17,7 @@ export const Button = ({ to, children, type, styleClass, onClick }) => {
       type={type}
       className={`btn ${styleClass}`}
       onClick={() => onClick()}
+      disabled={disabled}
     >
       {children}
     </button>
@@ -21,12 +29,13 @@ Button.propTypes = {
   children: propTypes.node.isRequired,
   type: propTypes.string,
   styleClass: propTypes.string,
-  onClick: propTypes.func,
+  onClick: propTypes.func.isRequired,
+  disabled: propTypes.string,
 };
 
 Button.defaultProps = {
   to: "",
-  type: "",
+  type: "button",
   styleClass: "",
-  onClick: () => {},
+  disabled: "",
 };
