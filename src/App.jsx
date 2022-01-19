cb95cb99ec6d3a2af5cbf39665c8dad1560ef673
Alicja Guzik <95084583+alicjaguzik@users.noreply.github.com>
2022-01-19 15:22:44 +0100

    fix: create button, reusable btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { AppRoutes } from "./routes/Routes";
 import { Footer } from "./components/footer";
-import { Button } from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Coderscamp</h1>
       <AppRoutes />
-      <Button />
       <Footer />
     </div>
   );
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,32 @@
-import styles from "./button.module.scss";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 
-export const Button = () => (
-  <button className={styles.button} type="submit">
-    Szukaj
-  </button>
-);
+export const Button = ({ to, children, type, styleClass, onClick }) => {
+  if (to) {
+    return <Link to={to}>{children}</Link>;
+  }
+  return (
+    <button
+      type={type}
+      className={`btn ${styleClass}`}
+      onClick={() => onClick()}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  to: propTypes.string,
+  children: propTypes.node.isRequired,
+  type: propTypes.string,
+  styleClass: propTypes.string,
+  onClick: propTypes.func,
+};
+
+Button.defaultProps = {
+  to: "",
+  type: "",
+  styleClass: "",
+  onClick: () => {},
+};
