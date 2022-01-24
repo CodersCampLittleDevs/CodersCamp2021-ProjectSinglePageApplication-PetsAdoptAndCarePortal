907c38de7407b6489b4fab9874f862140f727090
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-01-24 12:38:35 +0100

    fix: some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { Routes } from "./routes/Routes";
 import { Navbar } from "./components/Navbar/Navbar";
+import { AppRoutes } from "./routes/Routes";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Routes />
+        <h1>Coderscamp</h1>
+        <AppRoutes />
+        <Footer />
       </Router>
     </div>
   );
