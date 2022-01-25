bcaee67ee907c745b64ff0a9c7f230a70b881b5d
MaciejP <m.ponicki1995@gmail.com>
2022-01-25 11:22:59 +0100

    feat: add new variable in createPageNumbers

--- src/utils/createPageNumbers.js
+++ src/utils/createPageNumbers.js
@@ -2,9 +2,10 @@ import { POSTS_PER_PAGE } from "../constants/pagination";
 
 export const createPageNumbers = (totalPosts) => {
   const pageNumbers = [];
+  const roundedPageNumbersUp = Math.ceil(totalPosts / POSTS_PER_PAGE);
 
   if (typeof totalPosts === "number") {
-    for (let i = 1; i <= Math.ceil(totalPosts / POSTS_PER_PAGE); i++) {
+    for (let i = 1; i <= roundedPageNumbersUp; i++) {
       pageNumbers.push(i);
     }
   }
