8218117cc59c1f9ef3f2d58a77597a6239bbead8
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-01-25 12:25:46 +0100

    fix: logo.png was moved to assets

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import logo from "./logo.png";
+import logo from "../../assets/image/logo/logo.png";
 
 export const Logo = ({ classes }) => {
   return <img className={classes} src={logo} alt="Logo" />;
