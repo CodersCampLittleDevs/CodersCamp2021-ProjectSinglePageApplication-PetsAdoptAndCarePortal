0f83024bf73d0148bb965286e4839510a54be6a0
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-01-25 14:42:12 +0100

    fix: fixed button - adds class on click

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import clsx from "clsx";
 import styles from "./navbar.module.scss";
 import { routes } from "../../routes/index";
 import { Menu } from "./Menu";
 
-const Hamburger = () => (
-  <div className={styles.hamburger} id={styles.hamburger}>
-    <span className={styles.line} />
-    <span className={styles.line} />
-    <span className={styles.line} />
-  </div>
-);
+let hamburgerClasses = styles.hamburger;
 
+const Hamburger = () => {
+  const [change, setChange] = useState(false);
+  const hamburgerClick = () => {
+    setChange(!change);
+    if (change === false) {
+      hamburgerClasses = clsx([styles.hamburger, styles.isActive]);
+    } else {
+      setChange(!change);
+      hamburgerClasses = styles.hamburger;
+    }
+  };
+  return (
+    <button
+      className={hamburgerClasses}
+      id={styles.hamburger}
+      onClick={hamburgerClick}
+      onKeyDown={hamburgerClick}
+    >
+      <span className={styles.line} />
+      <span className={styles.line} />
+      <span className={styles.line} />
+    </button>
+  );
+};
 export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
