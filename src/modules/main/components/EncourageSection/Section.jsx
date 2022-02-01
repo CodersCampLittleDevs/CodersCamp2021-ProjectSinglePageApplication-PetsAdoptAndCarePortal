41652ece20a1603c210cf18ce380d006a5ee6736
MaciejP <m.ponicki1995@gmail.com>
2022-02-01 23:24:46 +0100

    WIP

--- src/modules/main/components/EncourageSection/Section.jsx
+++ src/modules/main/components/EncourageSection/Section.jsx
@@ -1,10 +1,22 @@
 import PropTypes from "prop-types";
 import styles from "./section.module.scss";
 
-export const Section = ({ imageSrc, title, description }) => {
+export const Section = ({ imageSrc, title, description, sectionSide }) => {
   return (
-    <section className={styles.section}>
-      <img className={styles.section__image} src={imageSrc} alt="" />
+    <section
+      className={
+        sectionSide === "true" ? styles.section__right : styles.section__left
+      }
+    >
+      <img
+        className={
+          sectionSide === "false"
+            ? styles.section__image_right
+            : styles.section__image_left
+        }
+        src={imageSrc}
+        alt=""
+      />
       <div className={styles.description}>
         <h3 className={styles.description__title}>{title}</h3>
         <p className={styles.description__text}>{description}</p>
@@ -16,5 +28,6 @@ export const Section = ({ imageSrc, title, description }) => {
 Section.propTypes = {
   imageSrc: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.objectOf(PropTypes.object).isRequired,
+  sectionSide: PropTypes.string.isRequired,
 };
--- src/modules/main/pages/Main.jsx
+++ src/modules/main/pages/Main.jsx
@@ -1,14 +1,49 @@
 import { Section } from "../components/EncourageSection/Section";
-import dog from "../../../assets/image/encourages/dog.jpg";
+import dog from "../../../assets/image/encourages/dog.png";
+import dog2 from "../../../assets/image/encourages/dog2.png";
 
 export const Main = () => {
   return (
     <div>
       <Section
+        sectionSide="true"
         imageSrc={dog}
         title="W trosce o potrzeby Twojego zwierzaka"
         description="Kompleksowa wyszukiwarka usług dla zwięrząt, która powstała z myślą 
         o czworonogach i ich właścicielach ułatwiając im dostęp do szerokiego pasma specjalistów."
+        imageSide="right"
+        descriptionSide="left"
+      />
+      <Section
+        sectionSide={false}
+        imageSrc={dog2}
+        title="Wszystkie usługi w jednym miejscu!"
+        description={
+          <>
+            Świadomość opiekunów w zakresie profilaktyki swoich zwierząt
+            regularnie wzrasta. Próbując sprostać wymaganiom i ułatwić
+            włascicielom czworonogów dostep do fachowej opieki oraz
+            profesjonalnych usług, stworzyliśmy platformę
+            <strong>PETS Adopt & Care.</strong>
+            <span>
+              <strong> Załóż konto</strong> już dziś by cieszyć się
+              nieograniczonym dostępem do szerokiego grona specjalistów i usług
+              jakie oferuje nasz portal.
+            </span>
+            <span>
+              <strong>PETS Adopt & Care</strong> to również świetny kanał
+              promocji swojej firmy, a zarazem prosty sposób umawiania wizyt dla
+              swoich podopiecznych.
+            </span>
+            <span>
+              <strong>Zarejestruj swoją firmę</strong> aby dołaćzyć do naszego
+              grona specjalistów i świadczyć usługi dla wszystkich miłośników
+              zwierząt!
+            </span>
+          </>
+        }
+        imageSide="left"
+        descriptionSide="right"
       />
     </div>
   );
