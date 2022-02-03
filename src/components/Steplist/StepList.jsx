643c8854159636efbeb43c757bedd4e8d596d9a2
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-02-03 13:27:12 +0100

    fix: add styles

--- src/components/Steplist/StepList.jsx
+++ src/components/Steplist/StepList.jsx
@@ -7,31 +7,35 @@ import step3 from "./images/step3.png";
 const STEPS = [
   {
     title: "Krok 1:",
-    description: "Skorzystaj z naszej wyszukiwarki",
+    description: "Skorzystaj z naszej",
+    description2: "wyszukiwarki",
     image: step1,
     alt: "step1",
   },
   {
     title: "Krok 2:",
-    description: "Znajdź placówkę w Twojej okolicy",
+    description: "Znajdź placówkę w Twojej",
+    description2: "okolicy",
     image: step2,
     alt: "step2",
   },
   {
     title: "Krok 3:",
-    description: "Wygodnie umów wizytę przez internet lub telefon",
+    description: "Wygodnie umów wizytę przez ",
+    description2: "internet lub telefon",
     image: step3,
     alt: "step3",
   },
 ];
 export const StepByStepList = () => (
   <div className={styles.step}>
-    <h1>W prosty i łatwy sposób wyszukasz interesujące Cię usługi</h1>
+    <h2>W prosty i łatwy sposób wyszukasz interesujące Cię usługi</h2>
     <div className={styles.steplist}>
       {STEPS.map((step) => (
         <StepByStepItem
           title={step.title}
           description={step.description}
+          description2={step.description2}
           image={step.image}
           key={step.title}
           alt={step.alt}
