0e29d2f1a69da2fb86c77a8c844413d9ec63ae05
Alicja Guzik <95084583+alicjaguzik@users.noreply.github.com>
2022-02-03 01:13:40 +0100

    styles for Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,40 +1,60 @@
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import styles from "./footer.module.scss";
+import logo from "../../assets/image/footer/Layer 2.png";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <img className={styles.logo} alt="logo" />
+      <div className={styles.logo_column}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <p className={styles.description}>
+          Wszystkie usługi dla Twojego pupila w jednym miejscu.
+        </p>
+        <p>Copyright &copy; 2022</p>
+      </div>
       <section className={styles.column}>
-        <header className={styles.header}>O nas</header>
+        <header className={styles.header}>Usługi dla zwierząt</header>
         <Router>
           <NavLink to="/" className={styles.link}>
-            Nasza misja
+            Weterynarz
           </NavLink>
+          <NavLink to="/" className={styles.link}>
+            Groomer
+          </NavLink>
+          <NavLink to="/" className={styles.link}>
+            Trener
+          </NavLink>
+          <NavLink to="/" className={styles.link}>
+            Behawiorysta
+          </NavLink>
+          <NavLink to="/" className={styles.link}>
+            Hotel dla zwierząt
+          </NavLink>
+          <NavLink to="/" className={styles.link}>
+            Sklep
+          </NavLink>
+        </Router>
+      </section>
+      <section className={styles.column}>
+        <header className={styles.header}>Konto</header>
+        <Router>
+          <NavLink to="/" className={styles.link}>
+            Zaloguj
+          </NavLink>
+          <NavLink to="/" className={styles.link}>
+            Zarejestruj
+          </NavLink>
+          <div className={styles.break} />
           <NavLink to="/" className={styles.link}>
             Kontakt
           </NavLink>
           <NavLink to="/" className={styles.link}>
             Regulamin
           </NavLink>
-        </Router>
-      </section>
-      <section className={styles.column}>
-        <header className={styles.header}>Wyszukaj</header>
-        <Router>
           <NavLink to="/" className={styles.link}>
-            Adopcje
-          </NavLink>
-          <NavLink to="/" className={styles.link}>
-            Hotele
-          </NavLink>
-          <NavLink to="/" className={styles.link}>
-            Usługi dla zwierząt
+            Polityka prywatności
           </NavLink>
         </Router>
-      </section>
-      <section className={styles.column}>
-        <header className={styles.header}>Social Media</header>
       </section>
     </footer>
   );
