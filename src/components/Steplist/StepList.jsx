08e38fb719f95b818c30c68ba4a8af110584bc7c
JustD1d1t <starzyk.dawid.0826@gmail.com>
2022-02-07 21:49:12 +0100

    refactor: move opinions and steps to constants

--- src/components/Steplist/StepList.jsx
+++ src/components/Steplist/StepList.jsx
@@ -1,32 +1,7 @@
 import { StepByStepItem } from "./StepItem";
 import styles from "./stepitem.module.scss";
-import step1 from "./images/step1.png";
-import step2 from "./images/step2.png";
-import step3 from "./images/step3.png";
+import { STEPS } from "../../constants/steps";
 
-const STEPS = [
-  {
-    title: "Krok 1:",
-    description: "Skorzystaj z naszej",
-    description2: "wyszukiwarki",
-    image: step1,
-    alt: "step1",
-  },
-  {
-    title: "Krok 2:",
-    description: "Znajdź placówkę w Twojej",
-    description2: "okolicy",
-    image: step2,
-    alt: "step2",
-  },
-  {
-    title: "Krok 3:",
-    description: "Wygodnie umów wizytę przez ",
-    description2: "internet lub telefon",
-    image: step3,
-    alt: "step3",
-  },
-];
 export const StepByStepList = () => (
   <div className={styles.step}>
     <h2>W prosty i łatwy sposób wyszukasz interesujące Cię usługi</h2>
