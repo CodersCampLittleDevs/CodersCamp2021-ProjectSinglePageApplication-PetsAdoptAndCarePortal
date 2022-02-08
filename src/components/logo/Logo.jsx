f44da4a105831119cc786a3e34815bdd37d1e43c
MaciejP <m.ponicki1995@gmail.com>
2022-02-08 18:46:51 +0100

    refactor: delete logo file

--- src/components/logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import PropTypes from "prop-types";
-import logo from "../../assets/image/logo/logo.png";
-
-export const Logo = ({ classes }) => {
-  return <img className={classes} src={logo} alt="Logo" />;
-};
-
-Logo.propTypes = { classes: PropTypes.string.isRequired };
