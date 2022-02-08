8d5cb34b5051e1f7bf051eb70866e06c13a91a42
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-02-08 12:09:11 +0100

    Fix: Added sample users

--- src/mock/auth.js
+++ src/mock/auth.js
@@ -1,5 +1,23 @@
 export const DUMMY_LOGINS = [
   {
+    id: 1,
+    name: "userTest",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat?",
+    announcement: [4, 5, 6, 7],
+    phoneNumber: "666 777 888",
+  },
+  {
+    id: 2,
+    name: "userName",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat?",
+    announcement: [1, 2, 3, 8, 9],
+    phoneNumber: "123 456 789",
+    company: "zwierzaki.pl",
+    openHours: "8-18",
+  },
+  {
     email: "test@example.com",
     username: "test",
     password: "test123",
