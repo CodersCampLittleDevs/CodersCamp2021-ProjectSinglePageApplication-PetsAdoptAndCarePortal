b0f160c9be38b49f28ff97fe303da6a93088bcb1
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-02-09 19:40:30 +0100

    feat: some styles

--- src/modules/announcements/pages/Announcement.jsx
+++ src/modules/announcements/pages/Announcement.jsx
@@ -16,42 +16,46 @@ export const Announcement = () => {
     (userItem) => userItem.id === announcement.userId,
   );
   return (
-    <section className={styles.announcement}>
-      <div className={styles.announcement__image}>
-        <img src={announcement.image} alt={announcement.title} />
-      </div>
-      <div className={styles.announcement__contactContainer}>
-        <div className={styles.announcement__contact}>
-          <p>{user?.name}</p>
-          <img src={user.image ? user.image : SampleUser} alt="" />
-          {user.company && (
-            <>
-              <p>{user.company}</p>
-            </>
-          )}
-          <Link to={`/announcements/user/${announcement.userId}`}>
-            Więcej od tego ogłoszeniodawcy
-          </Link>
-          <p className={styles.annoucemenet__phoneNumber}>{phoneNumber}</p>
-          <Button
-            type="button"
-            onClick={() => setPhoneNumber(user?.phoneNumber)}
-          >
-            Pokaż numer telefonu
-          </Button>
+    <div className={centered_Aannouncement}>
+      <section className={styles.announcement}>
+        <div className={styles.announcement__image}>
+          <img src={announcement.image} alt={announcement.title} />
         </div>
-      </div>
-      <div className={styles.announcement__details}>
-        <h2 className={styles.announcement__title}>{announcement.title}</h2>
-        <p className={styles.announcement__price}>Cena: {announcement.price}</p>
-        <p className={styles.announcement__rate}>
-          Ocena ogłoszenia: {announcement.rate}
-        </p>
-        <h3>Opis</h3>
-        <p className={styles.announcemenet__description}>
-          {announcement.description}
-        </p>
-      </div>
-    </section>
+        <div className={styles.announcement__contactContainer}>
+          <div className={styles.announcement__contact}>
+            <p>{user?.name}</p>
+            <img src={user.image ? user.image : SampleUser} alt="" />
+            {user.company && (
+              <>
+                <p>{user.company}</p>
+              </>
+            )}
+            <Link to={`/announcements/user/${announcement.userId}`}>
+              Więcej od tego ogłoszeniodawcy
+            </Link>
+            <p className={styles.annoucemenet__phoneNumber}>{phoneNumber}</p>
+            <Button
+              type="button"
+              onClick={() => setPhoneNumber(user?.phoneNumber)}
+            >
+              Pokaż numer telefonu
+            </Button>
+          </div>
+        </div>
+        <div className={styles.announcement__details}>
+          <h2 className={styles.announcement__title}>{announcement.title}</h2>
+          <p className={styles.announcement__price}>
+            Cena: {announcement.price}
+          </p>
+          <p className={styles.announcement__rate}>
+            Ocena ogłoszenia: {announcement.rate}
+          </p>
+          <h3>Opis</h3>
+          <p className={styles.announcemenet__description}>
+            {announcement.description}
+          </p>
+        </div>
+      </section>
+    </div>
   );
 };
