c1bc8027bdca0968a7c5057d96e1876409e07595
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-02-09 23:07:34 +0100

    fix: some fixes

--- src/modules/announcements/components/AnnouncementsList.jsx
+++ src/modules/announcements/components/AnnouncementsList.jsx
@@ -1,34 +1,38 @@
 import ReactStars from "react-rating-stars-component";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import styles from "./AnnouncementsList.module.scss";
+import styles from "./announcements_list.module.scss";
 
+const ANNOUNCEMENT_ROUTE = "/announcements/";
+const AnnouncementListEmptyState = () => (
+  <h3 className={styles.announcementNotFound}>
+    Przepraszamy, niestety nie ma żadnych ogłoszeń spełniających kryteria Twoich
+    wyszukiwań
+  </h3>
+);
 export const AnnouncementsList = ({ filteredAnnouncements }) => {
-  if (filteredAnnouncements.length === 0) {
-    return (
-      <h3 className={styles.announcementNotFound}>
-        Przeprazamy, niestety nie ma żadnych ogłoszeń spełniających kryteria
-        Twoich wyszukiwań
-      </h3>
-    );
+  if (!filteredAnnouncements.length) {
+    return <AnnouncementListEmptyState />;
   }
   return (
     <div className={styles.allAnnouncements}>
       <ul>
-        {filteredAnnouncements.map((e) => (
-          <li key={e.id}>
-            <Link to={`/announcements/ ${+e.id}`}>
+        {filteredAnnouncements.map((announcement) => (
+          <li key={announcement.id}>
+            <Link to={`${ANNOUNCEMENT_ROUTE}${+announcement.id}`}>
               <div className={styles.singleAnnouncement}>
                 <h3 className={styles.name}>
-                  {e.company ? e.company : e.title}
+                  {announcement.company
+                    ? announcement.company
+                    : announcement.title}
                   <br />
-                  {e.city}
+                  {announcement.city}
                 </h3>
-                <p className={styles.description}>{e.description}</p>
+                <p className={styles.description}>{announcement.description}</p>
                 <div className={styles.rating}>
                   <ReactStars
                     size={34}
-                    value={e.rate}
+                    value={announcement.rate}
                     edit={false}
                     activeColor="rgba(240, 140, 62, 1)"
                   />
@@ -45,9 +49,3 @@ export const AnnouncementsList = ({ filteredAnnouncements }) => {
 AnnouncementsList.propTypes = {
   filteredAnnouncements: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-// RatingStars.propTypes = {
-//   Rate: PropTypes.number,
-// };
-// RatingStars.defaultProps = {
-//   Rate: 0,
-// };
