b48964fe53af8d9c15699252754cac984e60ccb0
Maciej Ponicki <72940506+MaciekPon@users.noreply.github.com>
2022-02-09 18:07:52 +0100

    Feature/cra 15 (#22)
    * Merge branch 'master' into feature/CRA-15
    
    * feat: add Encourage sectionto main page
    add another dog image
    add Section component
    
    * fix: change file name
    
    * feat: add section to main page
    add styles for section.jsx
    
    * feat: add changes from code review
    change image name in assets
    
    * fix: change images names
    
    * fix: change image name in encourage section

--- src/modules/main/pages/Main.jsx
+++ src/modules/main/pages/Main.jsx
@@ -1,14 +1,16 @@
-import { EncourageSection } from "../components/EncourageSection/EncourageSection";
+import { WelcomeSection } from "../components/WelcomeSection/WelcomeSection";
 import { DividerSection } from "../../../components/DividerSection/DividerSection";
 import { ServicesList } from "../../../components/ServicesList/ServicesList";
 import { OpinionsSection, SearchForm } from "../../../components";
+import { EncourageSection } from "../components/EncourageSection/EncourageSection";
 
 export const Main = () => {
   return (
     <div>
-      <EncourageSection />
+      <WelcomeSection />
       <SearchForm />
       <ServicesList />
+      <EncourageSection />
       <OpinionsSection />
       <DividerSection />
     </div>
